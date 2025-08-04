ed1040f2b7a0f1dbbaf5b2791d535de937f6ec0e
dhanJi_exp <kdhanji183@gmail.com>
2025-08-04 15:07:15 +0530

    fix:hydration issue

--- src/components/dashboard/ShowQrCode.jsx
+++ src/components/dashboard/ShowQrCode.jsx
@@ -13,11 +13,11 @@ export default function ShowQrCode({ url, state }) {
                     </div>
                     <div className="md:h-80 h:50 md:w-full w-50 rounded-2xl relative">
 
-                        <p className="h-full w-8  absolute hidden md:block top-0 left-0 border-r border-slate-700">
+                        <div className="h-full w-8  absolute hidden md:block top-0 left-0 border-r border-slate-700">
                             <div className="w-ful pt-2 h-full text-center ">
                                 <span className="">D.</span>
                             </div>
-                        </p>
+                        </div>
 
                         <p className="w-full h-8  absolute hidden md:block top-0 right-0 border-b border-slate-700">
 
@@ -25,11 +25,11 @@ export default function ShowQrCode({ url, state }) {
                         <p className="h-full w-8  absolute hidden md:block top-0 right-0 border-l border-slate-700">
 
                         </p>
-                        <p className="w-full h-8  absolute hidden md:block bottom-0 right-0 border-t border-slate-700">
+                        <div className="w-full h-8  absolute hidden md:block bottom-0 right-0 border-t border-slate-700">
                             <div className="w-ful pt-2 h-full text-center ">
                                 <span className="uppercase">shortly.by</span>
                             </div>
-                        </p>
+                        </div>
 
                         <div className="w-full h-full md:p-8 p-4">
                             <div className="bg-green-400 w-full h-full">
